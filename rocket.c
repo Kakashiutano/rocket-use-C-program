7b5bc3ee69477a6225f4d8183c7881aac5e37d6d
Kakashiutano <47592198+Kakashiutano@users.noreply.github.com>
2019-03-18 08:10:01 +0700

    I've added the codes
    a spaceship rocketing through the sky

--- rocket.c
+++ rocket.c
@@ -1,1 +1,37 @@
 // I want to create active rocket
+#include <stdio.h>
+#include <unistd.h> // for usleep function
+ 
+const char rocket[] =
+"           _\n\
+          /^\\\n\
+          |-|\n\
+          |C|\n\
+          |H|\n\
+          |H|\n\
+          |U|\n\
+          |N|\n\
+          |E|\n\
+          |N|\n\
+          |G|\n\
+          | |\n\
+         /| |\\\n\
+        / | | \\\n\
+       |  | |  |\n\
+        `-\"\"\"-`\n\
+";
+ 
+int main()
+{
+	int i;
+    for (i = 0; i < 50; i ++) printf("\n"); // jump to bottom of console
+    printf("%s", rocket);
+    int j = 300000;
+    for (i = 0; i < 50; i ++) {
+        usleep(j); // move faster and faster,
+        j = (int)(j * 0.9); // so sleep less each time
+        printf("\n"); // move rocket a line upward
+    }
+    printf("Visit http://ChhunnengOS.com !\n");
+    return 0;
+}
